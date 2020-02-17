e94fc42d09f0536bfe972b76166aa34403502b16
Shobhit Ratan <shobhit.ratan@baruchmail.cuny.edu>
2020-02-17 22:59:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,13 @@ function theBeetlesPlay() {
   }
   return array;
 }
+
+function johnLennonFacts(facts) {
+  var array = [];
+  var i = 0;
+  while (i < facts.length) {
+    array.push (`${facts[i]}!!!`);
+    i++;
+  }
+  return array;
+}
