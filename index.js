5c1a4509d17671c1550e2977bb0330ab3a82f1cc
Shobhit Ratan <shobhit.ratan@baruchmail.cuny.edu>
2020-02-17 22:56:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function theBeetlesPlay() {
   let musicians = ['John Lennon', 'Paul McCartney', 'George Harrison', 'Ringo Starr'];
   let instruments = ['Guitar', 'Bass Guitar', 'Lead Guitar', 'Drums'];
   for (let i = 0; i < 4; i+= 1) {
-    array.push (i === 0 ? `${musicians[i]} plays ${instruments[i]}` : `${musicians[i]} plays ${instruments[i]}`);
+    array.push (i == 0 ? `${musicians[i]} plays ${instruments[i]}` : `${musicians[i]} plays ${instruments[i]}`);
   }
   return array;
 }
