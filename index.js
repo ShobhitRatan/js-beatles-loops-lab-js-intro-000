e307e43e1b58b600f68df0f2dd1b7d19fb515d8f
Shobhit Ratan <shobhit.ratan@baruchmail.cuny.edu>
2020-02-17 22:55:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // add solution here
+function theBeetlesPlay() {
+  var array = [];
+  let musicians = ['John Lennon', 'Paul McCartney', 'George Harrison', 'Ringo Starr'];
+  let instruments = ['Guitar', 'Bass Guitar', 'Lead Guitar', 'Drums'];
+  for (let i = 0; i < 4; i+= 1) {
+    array.push (i === 0 ? `${musicians[i]} plays ${instruments[i]}` : `${musicians[i]} plays ${instruments[i]}`);
+  }
+  
+}
